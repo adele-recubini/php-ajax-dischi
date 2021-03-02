6b8a3b06eeebcc1f8129aa3126b3bc89f6965267
adele <adele.r87@gmail.com>
2021-03-02 17:24:42 +0100

    modifiche

--- app/server.php
+++ app/server.php
@@ -1,3 +1,3 @@
-
-header('Content-Type: application/json');
-echo json_encode($dischi);
+<?php require_once __DIR__ . '/database/database.php'?>
+<?php   header('Content-Type: application/json') ?>
+<?php echo json_encode($dischi) ?>
